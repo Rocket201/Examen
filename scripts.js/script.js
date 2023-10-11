173a04fa09c1ec78548ed1580ab9dab38fbe0722
Rocket201 <alejandrocorroto99@gmail.com>
2023-10-11 10:44:36 +0200

    termino el programa

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -6,31 +6,31 @@ document.addEventListener("DOMContentLoaded", function(){
    // Creamos un addEvenListener que escuche el click del usuario.
      document.addEventListener("click", function(){
 
-        //Recogemos por el id y usando parseFloat para que sea numérico tanto la longitud como el ancho
+        //Recogemos por el id y usando parseFloat para que sea numérico tanto la longitud como el ancho.
         const longitud = parseFloat(document.getElementById("longitud").value)
         const ancho = parseFloat(document.getElementById("ancho").value)
        
-         // Lo filtramos con !isNaN para asegurarnos de que sean números(is not a number)
+         // Lo filtramos con !isNaN para asegurarnos de que sean números(is not a number).
         if(!isNaN (longitud) && !isNaN (ancho)){
           
-        // Realizamos las operaciones pertinentes y generamos el area el perimetro y la diagonal
-          area = longitud * ancho;
-
+        // Realizamos las operaciones pertinentes y generamos el area el perimetro y la diagonal.
+            area = longitud * ancho;
         /*Le decimos que salida necesita llamar desde el HTML en este caso la salida 1
-            se podria sacar todo por una única salida */
-            salida1.textContent= "Áreal del rectángulo:" + area;
+            se podria sacar todo por una única salida. */
+                salida1.textContent= "Áreal del rectángulo:" + area;
+                
+            perímetro = 2*(longitud + ancho);
 
-          perímetro = 2*(longitud + ancho);
+                salida2 .textContent= "Perímetro del rectángulo:" + perímetro;
 
-            salida2 .textContent= "Perímetro del rectángulo:" + perímetro;
             diagonal =  2*((longitud*2)+(ancho*2));
-            //diagonal = Math. sqrt((longitud*2)+(ancho*2));
-            salida3.textContent="Diagonal del rectángulo:" + diagonal;
 
-            // Finalmente la informacion la ha recibido el html y la ha mostrado
+                salida3.textContent="Diagonal del rectángulo:" + diagonal;
+
+            // Finalmente la informacion la ha recibido el html y la ha mostrado.
         } else{
-            // Cuando detecte que no hay valores válidos aparecera este mensaje
-                salida1.textContent="Introduce un dato válido"
+            // Cuando detecte que no hay valores válidos aparecerá este mensaje.
+                salida1.textContent="Introduce el Largo y el Ancho"
         }
      });
 
